fe7bb679d02ed236c3b5bcc4de1ffe4f9e734da1
PedroHMantis <gtx1050pd@gmail.com>
2025-08-28 15:22:29 -0300

    Exception handling - update

--- src/main/java/com/pedromantis/course/services/UserService.java
+++ src/main/java/com/pedromantis/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.pedromantis.course.entities.User;
 import com.pedromantis.course.repositories.UserRepository;
 import com.pedromantis.course.services.exceptions.DatabaseException;
 import com.pedromantis.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -48,9 +49,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
