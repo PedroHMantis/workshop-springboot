7a8d876121644599027b837aa182014471e4efcb
PedroHMantis <gtx1050pd@gmail.com>
2025-08-30 16:19:32 -0300

    CORS_ORIGINS

--- src/main/java/com/pedromantis/course/config/WebConfig.java
+++ src/main/java/com/pedromantis/course/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.pedromantis.course.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,18 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-    @Value("${cors.origins}")
-        private String corsOrigins;
-
-        @Bean
-        public WebMvcConfigurer corsConfigurer() {
-            return new WebMvcConfigurer() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
-                }
-            };
-        }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Permite apenas sua URL de produção
+                registry.addMapping("/**")
+                        .allowedOrigins("https://workshop-sb-prod.up.railway.app")
+                        .allowedMethods("*");
+            }
+        };
     }
+
 }
